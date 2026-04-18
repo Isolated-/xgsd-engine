8229e81cc83391ed4726f396864843ce28a4f983
Michael Palmer <michaelpalmer53@gmail.com>
2026-04-18 06:05:51 +0100

    fixes typo in FactoryInput

--- src/types/core/factory.ts
+++ src/types/core/factory.ts
@@ -1,4 +1,4 @@
 import {Context} from '../context'
 
 export type Factory<T> = (ctx: Context) => T
-export type FactoryInput<T> = T | Factory<T> | (new (ctx: Context) => Factory<T>)
+export type FactoryInput<T> = T | Factory<T> | (new (ctx: Context) => T)

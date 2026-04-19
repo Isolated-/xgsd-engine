46188d72495f4a80aec304e0edef55f00c0fb197
Michael Palmer <michaelpalmer53@gmail.com>
2026-04-19 04:09:06 +0100

    fixes ts build issues preventing proper import externally

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,9 @@
-import type { Config } from "@jest/types";
+export default {
+  preset: 'ts-jest/presets/default-esm',
 
-const config: Config.InitialOptions = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  testMatch: ["**/__tests__/**/*.test.ts"],
-  moduleFileExtensions: ["ts", "js", "json", "node"],
-  collectCoverage: false,
-  collectCoverageFrom: ["src/**/*.ts"],
-};
+  extensionsToTreatAsEsm: ['.ts'],
 
-export default config;
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+}
---
+++ src/__tests__/index.test.ts
@@ -0,0 +1,5 @@
+import {someFunction} from '../index.js'
+
+test('do something', () => {
+  expect(someFunction(1)).toBe(2)
+})

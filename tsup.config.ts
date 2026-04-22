cb683719390944384300e795b04489c4be76e82f
Michael Palmer <michaelpalmer53@gmail.com>
2026-04-22 01:11:26 +0100

    only builts required deps + removes unneeded stuff from package

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'tsup'
+import {defineConfig} from 'tsup'
 
 export default defineConfig({
-  entry: ['src/*.ts'],
+  entry: ['src/index.ts'],
   format: ['esm', 'cjs'],
   dts: true,
   clean: true,
   sourcemap: true,
   target: 'es2022',
-  external: ['jest']
+  external: ['jest'],
 })
